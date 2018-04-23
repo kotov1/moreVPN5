e2f1560c35f78fc3f650b504824ed8c234c49b10
Petr Petukhov <kotov0991@gmail.com>
2018-04-23 15:43:04 +0300

    ready for test

--- js/scripts.js
+++ js/scripts.js
@@ -259,6 +259,16 @@ $(function() {
 				},
 			}
 		});
+
+
+
+	$('.js-redirect').click(function(event) {
+		event.preventDefault;
+		console.log('cl');
+		if($(this).closest('form').valid()) {
+			location.replace("payment.html");
+		}
+	});
 	
 
 });
